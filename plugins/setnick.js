0beb07b2fdb0592db5de27cf74bc5daae1421039
Eegras <eegras@eegrasstudios.com>
2020-08-30 09:04:11 -0500

    Allow setnick.js to clear a user's nickname.

--- plugins/setnick.js
+++ plugins/setnick.js
@@ -5,18 +5,34 @@ const utils = require('../src/utils');
 // Usage:
 //   !setnick <new nickname>
 // Must be run inside an inbox thread.  It will set the requester's nickname the
-//mainGuild and tells the user anonymously.  
+// mainGuild and tells the user anonymously.
+// If <new nickname> is not provided it removes the user's nickname.
 // It does not handle errors well at all.
 
 module.exports = function({ bot, knex, config, commands }) {
   commands.addInboxThreadCommand('setnick', '[nickname$]', (msg, args, thread) => {
+  	if (! args.nickname ) {
+      newnick = "";
+    } else {
+      newnick = args.nickname;
+    }
+
     try {
-      var changenick = bot.editGuildMember(config.mainGuildId, thread.user_id, { nick: args['nickname'] }, "Requested in modmail");
+      var changenick = bot.editGuildMember(config.mainGuildId, thread.user_id, {nick: newnick}, "Requested in modmail");
       changenick.then((result) => {
-        thread.replyToUser(msg.member, "Your nickname has been set to " + args['nickname'], [], true);
-      });
-    } catch (err) {
-      thread.postSystemMessage("Generic error applying setnick: " + err);
+        if ( newnick == "" )
+        {
+          thread.replyToUser(msg.member, "Your nickname has been cleared.", [], true);
+        }
+        else
+        {
+          thread.replyToUser(msg.member, "Your nickname has been set to "+args['nickname'], [], true);
+        }
+      })
+    }
+    catch(err) {
+      thread.postSystemMessage( "Generic error applying setnick: "+err);
+      //utils.postError(msg.channel, "Generic error applying setnick: "+err);
     }
   });
 }
