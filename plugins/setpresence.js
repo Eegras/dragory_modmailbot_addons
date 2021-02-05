2b0574a40a3073f327269ae778387b16f3c62b5f
Eegras <eegras@eegrasstudios.com>
2021-02-05 14:41:26 -0600

    Initial Setup

---
+++ plugins/setpresence.js
@@ -0,0 +1,15 @@
+// https://github.com/Eegras/dragory_modmailbot_addons/blob/master/plugins/setpresence.js
+
+// Usage:
+//   !setpresence <state>
+//   <state> can be one of:
+//      invisible
+//      dnd
+//      away
+//      online
+
+module.exports = function({ bot, knex, config, commands }) {
+  commands.addInboxThreadCommand('setpresence', '[presence$]', (msg, args, thread) => {
+    bot.editSelfSettings({'status': args.presence});
+  });
+}
